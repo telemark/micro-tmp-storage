703126e9b9c0230aa70ccc4ebc4a873b4902d84c
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-02-01 10:30:38 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
+[![Build Status](https://travis-ci.org/telemark/micro-tmp-storage.svg?branch=master)](https://travis-ci.org/telemark/micro-tmp-storage)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+
 # micro-tmp-storage
-Microservice for storing data temporarily
+
+Microservice for storing data temporarily.
+
+Default ttl is 60 seconds
+
+## API
+
+### POST
+
+**value** value to store
+**ttl** ttl in seconds (optional, defaults to 60)
+
+```bash
+$ curl -d '{"value": "My secret, temporary value", "ttl": "30"}' https://tmp.storage.t-fk.win
+```
+
+returns
+
+```JavaScript
+{
+  key: 'b72545e8-6b58-4672-ad87-4096bb9da778',
+  value: 'My secret, temporary value',
+  ttl: 30,
+  success: true
+}
+```
+
+### GET
+
+#### ```/{key}```
+
+```bash
+$ curl -d https://tmp.storage.t-fk.win/b72545e8-6b58-4672-ad87-4096bb9da778
+```
+
+returns 200 ok on success
+
+```JavaScript
+{
+  key: 'b72545e8-6b58-4672-ad87-4096bb9da778',
+  value: 'My secret, temporary value'
+}
+```
+
+returns 404 not found on failure
+
+```JavaScript
+{
+  key: 'b72545e8-6b58-4672-ad87-4096bb9da778',
+  value: false
+}
+```
+
+## License
+
+[MIT](LICENSE)
+
+![alt text](https://robots.kebabstudios.party/micro-tmp-storage.png "Robohash image of micro-tmp-storage")
---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const readFileSync = require('fs').readFileSync
+const NodeCache = require('node-cache')
+const cache = new NodeCache()
+const marked = require('marked')
+const uid = require('uuid/v4')
+const { parse } = require('url')
+const { send, json } = require('micro')
+
+module.exports = async (request, response) => {
+  const {pathname, query} = await parse(request.url, true)
+  const data = request.method === 'POST' ? await json(request) : query
+
+  if (request.method === 'POST') {
+    const value = data.value || ''
+    const ttl = data.ttl ? parseInt(data.ttl, 10) : 60
+    const key = uid()
+    const success = cache.set(key, value, ttl)
+    const result = {
+      key: key,
+      value: value,
+      ttl: ttl,
+      success: success
+    }
+
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'GET')
+
+    send(response, 200, result)
+  } else if (pathname !== '/') {
+    const key = pathname.replace('/', '')
+    const value = cache.get(key)
+    const code = value ? 200 : 404
+    const result = {
+      key: key,
+      value: value || false
+    }
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'GET')
+    send(response, code, result)
+  } else {
+    const readme = readFileSync('./README.md', 'utf-8')
+    const html = marked(readme)
+    send(response, 200, html)
+  }
+}
