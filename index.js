c744818bc4100a3057bf713099d313c5d455fd17
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-04-08 22:20:20 +0200

    Adds default for / (minor)

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const logger = require('./lib/logger')
 module.exports = async (request, response) => {
   const pathname = request.url
   const data = request.method === 'POST' ? await json(request) : await request.query
-
   if (request.method === 'POST') {
     const value = data.value || ''
     const ttl = data.ttl ? parseInt(data.ttl, 10) : 60
@@ -26,15 +25,22 @@ module.exports = async (request, response) => {
     send(response, 200, result)
   } else if (pathname !== '/') {
     const key = /storage/.test(pathname) ? pathname.replace('/storage/', '') : pathname.replace('/', '')
-    const data = await getFromStorage(key)
-    const code = data !== null ? 200 : 404
-    const result = {
-      key: key,
-      value: data !== null ? data.value : false
+    if (key) {
+      const data = await getFromStorage(key)
+      const code = data !== null ? 200 : 404
+      const result = {
+        key: key,
+        value: data !== null ? data.value : false
+      }
+      response.setHeader('Access-Control-Allow-Origin', '*')
+      response.setHeader('Access-Control-Allow-Methods', 'GET')
+      logger('info', ['index', 'GET', 'key', key, 'code', code])
+      send(response, code, result)
+    } else {
+      logger('info', ['index', 'GET', 'no key, please visit docs'])
+      send(response, 200, 'Please visit /docs')
     }
-    response.setHeader('Access-Control-Allow-Origin', '*')
-    response.setHeader('Access-Control-Allow-Methods', 'GET')
-    logger('info', ['index', 'GET', 'key', key, 'code', code])
-    send(response, code, result)
+  } else {
+    send(response, 200, 'Please visit /docs')
   }
 }
